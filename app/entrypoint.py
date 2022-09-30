81db3e2f539d8a244a0fb86231c9dc4ff926c7de
Laurens van der Tas <Laurens.vanderTas@ad.dfnld.nl>
2022-09-30 11:21:47 +0200

    Add: Application entrypoint script

---
+++ app/entrypoint.py
@@ -0,0 +1,24 @@
+from src.config_handling import load_config
+from src.pipeline_components import (
+    load_data,
+    one_hot,
+    time_components,
+    impute,
+    return_and_serialize,
+)
+
+if __name__ == "__main__":
+
+    # Load config
+    config = load_config(file="./config/config.yaml")
+
+    # Run pipeline
+    data_transformed = (
+        load_data(
+            "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/planets.csv"
+        )
+        .pipe(one_hot, "method", config["methods"])
+        .pipe(time_components, "year", config["time-components"])
+        .pipe(impute, "linear")
+        .pipe(return_and_serialize, "./dump/output/transformed.json")
+    )
